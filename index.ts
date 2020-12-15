f37ec50307d0005f8a5efa333281023cc6ff1149
lyon <lyon19930914@gmail.com>
2020-12-15 13:50:10 +0800

    feat:add 递归form表单字段

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@
  * @param {number} size
  * @returns
  */
-const fileSizeCount = (size: number) => {
+export const fileSizeCount = (size: number) => {
   const kb = Math.floor((Number(size) / 1024) * 10) / 10;
   const mb = kb / 1024;
   const gb = mb / 1024;
@@ -18,4 +18,47 @@ const fileSizeCount = (size: number) => {
   } else {
     return `${kb} KB`;
   }
+};
+
+/**
+ *递归form表单字段
+ *
+ * @param {*} obj
+ * @returns
+ */
+const recursionFormFields = (obj: any) => {
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      const fieldVal = obj[key];
+      if (Object.prototype.toString.call(fieldVal) === "[object String]") {
+        // string转成boolean,回显
+        if (key === "symptom_1") {
+          if (fieldVal === "0") {
+            obj[key] = false;
+          } else if (fieldVal === "1") {
+            obj[key] = true;
+          }
+        }
+      }
+      if (Object.prototype.toString.call(fieldVal) === "[object Boolean]") {
+        // 所有value为boolean类型转成'0','1'
+        fieldVal ? (obj[key] = "1") : (obj[key] = "0");
+      } else if (Object.prototype.toString.call(fieldVal) === "[object Null]") {
+        // 过滤所有值为null的字段
+        obj[key] = undefined;
+      } else if (key === "de_dob") {
+        // 所有日期为string时,转为Moment对象
+        obj[key] = moment(fieldVal);
+      } else if (
+        Object.prototype.toString.call(fieldVal) === "[object Array]"
+      ) {
+        fieldVal.map((item: any) => {
+          if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            recursionFormFields(item);
+          }
+        });
+      }
+    }
+  }
+  return obj;
 };
